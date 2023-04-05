458e879b73d10d3a3f7417cbc49856165516c5ee
Helina Alebachew <helinaabebe42@gmail.com>
2023-04-05 19:05:10 +0900

    Add structure of todolist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const todolist = [
+  {
+    index: 0,
+    description: 'laundry',
+    completed: false,
+  },
+  {
+    index: 1,
+    description: 'work out',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'study',
+    completed: false,
+  },
+];
+const list = document.querySelector('.list');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
-}
-
-document.body.appendChild(component());
+const component = () => {
+  const newlist = document.createElement('section');
+  newlist.classList.add('todolist');
+  for (let i = 0; i < todolist.length; i++) {
+  // todolist.forEach((todo) => {
+    newlist.innerHTML = ` 
+          <div class ="dolist">
+              <input type="checkbox" id="listitem"/>
+              <label for="listitem"> ${todolist[i].description}</label>
+          </div>
+          <i id =liimg></i>`;
+  }
+  list.appendChild(newlist);
+};
+window.addEventListener('load', component);
